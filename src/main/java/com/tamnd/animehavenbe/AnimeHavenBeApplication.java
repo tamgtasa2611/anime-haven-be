8ecce165c5856ba20f2e3135a4b6816e4bea0e91
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2024-08-06 18:33:56 +0700

    init

---
+++ src/main/java/com/tamnd/animehavenbe/AnimeHavenBeApplication.java
@@ -0,0 +1,13 @@
+package com.tamnd.animehavenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeHavenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeHavenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamnd/animehavenbe/AnimeHavenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamnd.animehavenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeHavenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
